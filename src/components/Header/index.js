416b80db8f5c1965d076ba76740937993885063b
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-05-15 18:13:12 -0300

    :lipstick: feat: Styling Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,41 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
-import { HeaderWrapper, NavItem, Brand } from './styles';
+import {
+  HeaderWrapper,
+  NavItem,
+  Brand,
+  NavWrapper,
+  NavMenu,
+  NavLink,
+} from './styles';
 
 const Header = ({ siteTitle }) => (
-  <HeaderWrapper>
-    <Container>
-      <Row>
-        <Col xs="4">
-          <Brand>{siteTitle}</Brand>
-        </Col>
-        <Col xs="4">
-          <nav>
-            <ul>
-              <NavItem>
-                <a href="#">Inicio</a>
-              </NavItem>
-              <NavItem>
-                <a href="#">Sobre</a>
-              </NavItem>
-              <NavItem>
-                <a href="#">Menu</a>
-              </NavItem>
-              <NavItem>
-                <a href="#">FAQ</a>
-              </NavItem>
-              <NavItem>
-                <a href="#">Contatos</a>
-              </NavItem>
-            </ul>
-          </nav>
-        </Col>
-      </Row>
-    </Container>
+  <HeaderWrapper id="inicio">
+    <Col xs="12" sm="4">
+      <Brand>{siteTitle}</Brand>
+    </Col>
+    <Col xs="12" sm="8">
+      <NavWrapper>
+        <NavMenu>
+          <NavItem>
+            <NavLink href="#inicio">Inicio</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Sobre</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Menu</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">FAQ</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Contatos</NavLink>
+          </NavItem>
+        </NavMenu>
+      </NavWrapper>
+    </Col>
   </HeaderWrapper>
 );
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -6,18 +6,77 @@ export const Brand = styled(Link)`
     font-size: 1.5rem;
     color: var(--support-color-1);
     font-family: var(--primary-font);
+    font-weight: 700;
+  }
+
+  &:hover {
+    color: var(--support-color-1);
   }
 `;
 
 export const HeaderWrapper = styled.header`
   & {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
     padding: var(--gap) 0;
-    align-items: center;
+  }
+`;
+
+export const NavWrapper = styled.nav`
+  & {
+    justify-self: flex-end;
+  }
+`;
+
+export const NavMenu = styled.ul`
+  & {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  @media (max-width: 768px) {
+    & {
+      display: block;
+    }
   }
 `;
 
 export const NavItem = styled.li`
   & {
     display: inline;
+    align-self: flex-end;
+    margin-left: var(--gap);
+  }
+
+  &:last-child a {
+    padding-right: 0;
+  }
+
+  &:first-child a {
+    padding-left: 0;
+  }
+
+  @media (max-width: 768px) {
+    & {
+      display: block;
+      padding: 0px;
+      margin: 0px;
+      border-top: 2px solid;
+    }
+  }
+`;
+
+export const NavLink = styled.a`
+  & {
+    letter-spacing: 0.05em;
+    color: var(--support-color-1);
+    transition: 0.2s;
+  }
+
+  &:hover {
+    color: var(--base-color);
+    transition: 0.2s;
   }
 `;
