8b8390f3916b0caaa655598fe260992ce9eefbc9
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-05-15 09:00:47 -0300

    :lipstick: feat: Footer component
    Created Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  )
+}
