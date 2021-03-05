0cd2b2bb62108ff40b7aba668893e59644858741
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-03-05 09:55:55 -0300

    :construction: chore: Input and Textarea components added

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,33 +1,22 @@
 import React from 'react';
 
+import Input from '../Input/Input';
+import Textarea from '../Textarea/Textarea';
+
 import * as F from './styles';
 
 export default function ContactForm() {
   return (
     <F.FormWrapper title="Entre em contato conosco">
       <h3>Entre em contato conosco</h3>
-      <label htmlFor="nome">Nome</label>
-      <input
-        type="text"
-        name="nome"
-        id="nome"
-        placeholder="Digite seu nome aqui"
-      />
 
-      <label htmlFor="email">E-mail</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Digite seu email aqui"
-      />
-
-      <label htmlFor="mensagem">Mensagem</label>
-      <textarea
-        nameme="mensagem"
-        id="mensagem"
+      <Input name="nome" label="Nome" placeholder="Digite seu nome aqui" />
+      <Input name="email" label="E-mail" placeholder="Digite seu email aqui" />
+      <Textarea
+        name="mensagem"
+        label="Mensagem"
         placeholder="Digite sua mensagem aqui"
-      ></textarea>
+      />
 
       <button type="submit">Enviar</button>
     </F.FormWrapper>
