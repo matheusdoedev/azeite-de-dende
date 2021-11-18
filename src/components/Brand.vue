f79d3a1fdd3276c93f9e2944e4d44143d2b40f4b
matheusdoedev <matheusdoe.dev@gmail.com>
2021-11-18 17:51:38 -0300

    feat: Brand component

---
+++ src/components/Brand.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="Brand" href="/">
+    <img src="~/assets/icons/azeite-de-dende.svg" alt="Azeite de Dênde" />
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class Brand extends Vue {}
+</script>
+
+<style lang="scss">
+.Brand {
+  display: block;
+  width: 191px;
+  height: 18px;
+}
+</style>
