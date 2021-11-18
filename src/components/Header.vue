9d0388f923968f3b07e98df15b5ab362a4a5746b
matheusdoedev <matheusdoe.dev@gmail.com>
2021-11-18 17:51:46 -0300

    feat: Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="Header">
+    <Brand />
+    <Nav />
+  </header>
+</template>
+
+<style lang=""></style>
+
+<script lang="ts">
+import Vue from "vue";
+
+import { Component } from "nuxt-property-decorator";
+
+const components = {
+  Brand: () => import("~/components/Brand.vue"),
+  Nav: () => import("~/components/Nav.vue"),
+};
+
+@Component({ components })
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss">
+@import "~/assets/styles/settings/gaps";
+
+.Header {
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  margin: 0 auto;
+  padding: $md;
+}
+</style>
