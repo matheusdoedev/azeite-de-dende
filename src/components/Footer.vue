998789a65bb6cecf589638934dff515c77ef24d7
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-16 17:53:59 -0300

    refactor: working in Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,6 +17,30 @@
           </ul>
         </nav>
       </section>
+      <section class="Footer__section">
+        <h3 class="Footer__section__title">Contatos</h3>
+        <ul class="Footer__contacts">
+          <li
+            class="Footer__contacts__item"
+            v-for="item in contacts"
+            v-bind:key="item.content"
+          >
+            - {{ item.content }}
+          </li>
+        </ul>
+      </section>
+      <section class="Footer__section">
+        <h3 class="Footer__section__title" style="margin-bottom: 0;">
+          Redes Sociais
+        </h3>
+        <SocialMedias />
+      </section>
+    </div>
+    <div class="Footer__copy__block">
+      <p class="Footer__copy">
+        Feito por matheusdoedev. Azeite de Dende 2020 @ Todos os direitos
+        reservados&copy;
+      </p>
     </div>
   </footer>
 </template>
@@ -33,15 +57,25 @@ const data = () => ({
     { label: 'Sobre', href: '#sobre' },
     { label: 'Contato', href: '#contato' },
   ],
+
+  contacts: [
+    { content: 'contato@azeitededende.com' },
+    { content: '(71) 99999 - 9999' },
+  ],
 });
 
-@Component({ data })
+const components = {
+  SocialMedias: () => import("~/components/SocialMedias.vue"),
+}
+
+@Component({ data, components })
 export default class Footer extends Vue {}
 </script>
 
 <style lang="scss">
 @import '~/assets/styles/settings/colors';
 @import '~/assets/styles/settings/gaps';
+@import '~/assets/styles/settings/fonts';
 @import '~/assets/styles/objects/container';
 
 .Footer {
@@ -51,5 +85,47 @@ export default class Footer extends Vue {}
 
 .Footer__container {
   @extend .Container;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: $sm;
+}
+
+.Footer__section__title {
+  color: $support-color-3;
+  margin-bottom: $md;
+}
+
+.Footer__nav__menu {
+  display: flex;
+  flex-direction: column;
+  row-gap: $xs;
+  color: #d9d9d9;
+}
+
+.Footer__nav__link {
+  padding-left: 0;
+  color: #d9d9d9;
+  font-family: $secondary-font;
+}
+
+.Footer__contacts {
+  @extend .Footer__nav__menu;
+}
+
+.Footer__contacts__item {
+  @extend .Footer__nav__link;
+}
+
+.Footer__copy__block {
+  @extend .Container;
+
+  align-items: center;
+}
+
+.Footer__copy {
+  text-align: center;
+  margin-top: $md;
+  color: #d9d9d9;
 }
 </style>
