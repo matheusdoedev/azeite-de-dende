e4174241c2f781e0bf8ac62432ea5fcd98502ba1
matheusdoedev <matheusdoe.dev@gmail.com>
2021-12-20 10:34:02 -0300

    feat: site metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,136 @@
+import { siteMetadata } from './site-metadata';
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "azeite-de-dende",
+    title: 'Azeite de Dênde',
     htmlAttrs: {
-      lang: "pt-BR",
+      lang: 'pt-BR',
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: siteMetadata.description,
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        property: `og:title`,
+        content: siteMetadata.title,
+      },
+      {
+        property: `og:description`,
+        content: siteMetadata.description,
+      },
+      {
+        property: `og:type`,
+        content: `website`,
+      },
+      {
+        name: `twitter:card`,
+        content: `summary`,
+      },
+      {
+        name: `twitter:creator`,
+        content: siteMetadata.author,
+      },
+      {
+        name: `twitter:title`,
+        content: siteMetadata.title,
+      },
+      {
+        name: `twitter:description`,
+        content: siteMetadata.description,
+      },
+      {
+        name: 'theme-color',
+        content: '#fdd204',
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#fdd204',
+      },
+      {
+        name: 'msapplication-TileImage',
+        content: '/ms-icon-144x144.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: '/apple-icon-57x57.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/apple-icon-60x60.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: '/apple-icon-72x72.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/apple-icon-76x76.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: '/apple-icon-114x114.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/apple-icon-120x120.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: '/apple-icon-144x144.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/apple-icon-152x152.png',
+      },
+
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-icon-180x180.png',
+      },
+
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/android-icon-192x192.png',
+      },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/manifest.json' },
+    ],
   },
 
-  srcDir: "src/",
+  srcDir: 'src/',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["src/assets/styles/index.scss"],
+  css: ['src/assets/styles/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -28,7 +141,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
+    '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
---
+++ site-metadata.ts
@@ -0,0 +1,8 @@
+export const siteMetadata = {
+  title: 'Azeite de Dênde',
+  description:
+    'Restaurante de comida baiana no Pelourinho, centro histórico da cidade de Salvador.',
+  author: '@matheusdoedev',
+  keywords:
+    'azeite, de, dende, restaurante, baiana, comida, Pelourinho, cidade, histórico, centro, matheusdoedev',
+};
